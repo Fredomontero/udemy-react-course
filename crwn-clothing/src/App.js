05f0ba84bd24fce012cce7e6a21c6cf899b015d6
Christian <christianmontero08@gmail.com>
2019-10-15 12:30:27 -0700

    Broken our homepage into smaller components

---
+++ crwn-clothing/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import HomePage from './pages/homepage/homepage.component'
+
+function App() {
+  return (
+    <HomePage></HomePage>
+  );
+}
+
+export default App;
